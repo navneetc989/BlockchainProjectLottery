43a2fa58d9033f51c505ceeb613e70107dcbac5d
navneetc989 <navneetchapke@gmail.com>
2022-12-01 15:04:07 +0530

    Add files via upload

--- contracts/LotteryBiased.sol
+++ contracts/LotteryBiased.sol
@@ -12,7 +12,7 @@ contract Lottery {
     }
 
     receive() external payable {
-        require(msg.value == 1 ether, "Please pay 0.1 ether only"); //change
+        require(msg.value == 1 ether, "Please pay 1 ether only"); //change
         players.push(payable(msg.sender));
     }
 
@@ -22,31 +22,49 @@ contract Lottery {
     }
 
     function random() internal view returns (uint256) {
+        uint size = 100;
         return
             uint256(
                 keccak256(
                     abi.encodePacked(
                         block.difficulty,
                         block.timestamp,
-                        players.length
+                        size
                     )
                 )
             );
     }
+/*
+    uint arrSize = (players.length*(players.length+1))/2;
+    uint[arrSize] public arr;
+    */
+
+    uint[] public arr;
+    uint count;
+    uint temp;
 
     function pickWinner() public {
         //change
+        //player length = players.length
+
+        //Creating an array of size n(n+1)/2
+      
         require(msg.sender == manager, "You are not the manager"); //change
         require(players.length >= 3, "Players are less than 3"); //change
 
+        for(uint i = 0; i < players.length; i++) {
+             temp = players.length-1-i;
+             while(temp>=0) {
+                 arr.push(i);
+                 count++;
+                 temp--;
+             }
+        }
         uint256 r = random();
-
-        uint256 index = r % players.length;
-
-        winner = players[index];
-
+        uint256 index = r % arr.length;
+        uint arrIndex = arr[index];
+        winner = players[arrIndex];
         winner.transfer(getBalance());
-
         players = new address payable[](0);
     }
 
@@ -56,6 +74,3 @@ contract Lottery {
 }
 
 
-//Contract add - 0x80191032fB4d309501d2EBc09a1A7d7F2941C8C1
-
-//Ganache contract add - 0x9B1D7bd063Be226c3FbafCEb43891552bD02DadE
